04ac0d38b1c45f33e1b551d987b611cbf60e42a3
DronKaustub <97157814+DronKaustub@users.noreply.github.com>
2025-06-11 12:50:43 +0530

    Time: 12 ms (33.7%), Space: 12.3 MB (52.89%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
@@ -28,3 +28,14 @@ public:
         //return node;
     }
 };
+/* WOWOW SOLn;
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        // Overwrite data of next node on current node.
+        node->val = node->next->val;
+        // Make current node point to next of next node.
+        node->next = node->next->next;
+    }
+};
+*/
