51ae336789517ec067524dd04c26bdfe8f08c3a4
DronKaustub <97157814+DronKaustub@users.noreply.github.com>
2025-06-11 12:19:23 +0530

    Time: 4 ms (52.81%), Space: 12.6 MB (12.23%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        # p=48
+        # n=len(num1)
+        # m=len(num2)
+        # sum1=sum2=1
+        # for i in range(0,n):
+        #     sum1=int (num1)
+        #     print(int(sum1)+p)
+        # for i in range(0,m):
+        #     sum2=num2[i]
+        #     print(sum2)
+        return (str(int(num1)*int(num2)))
+
+
