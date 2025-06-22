135c87e70aca7eddd363f794139b428b59a1f13d
Dron Kaustub <97157814+DronKaustub@users.noreply.github.com>
2025-06-22 14:42:00 +0530

    Time: 0 ms (100%), Space: 18 MB (17.89%) - LeetHub

--- 2260-divide-a-string-into-groups-of-size-k/2260-divide-a-string-into-groups-of-size-k.py
+++ 2260-divide-a-string-into-groups-of-size-k/2260-divide-a-string-into-groups-of-size-k.py
@@ -1,20 +1,33 @@
 class Solution:
     def divideString(self, s: str, k: int, fill: str) -> List[str]:
-        a=[]
-        n= len(s)
+        
+        # better soln....
+        remainder = len(s) % k
+        if remainder != 0:
+            s += fill * (k - remainder)
+        
+        # Use list comprehension to build the result in chunks
+        return [s[i:i+k] for i in range(0, len(s), k)]
+        
+        
+        
+        # my soln below O(kN)
+        
+        # a=[]
+        # n= len(s)
 
-        p= n%k
-        m=0
-        for i in range(0,n,k):
-            dk=""
-            for j in range(0,k):
-                if(i+j>=n):
-                    dk+=fill
-                else:
-                    dk+=s[i+j]
+        # p= n%k
+        # m=0
+        # for i in range(0,n,k):
+        #     dk=""
+        #     for j in range(0,k):
+        #         if(i+j>=n):
+        #             dk+=fill
+        #         else:
+        #             dk+=s[i+j]
 
-            a.append(dk)
+        #     a.append(dk)
            
-            m+=1
+        #     m+=1
 
-        return a
+        # return a
