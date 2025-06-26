34e5f3b8b0a1dd4f52775212dbeff11928c06d8b
Dron Kaustub <97157814+DronKaustub@users.noreply.github.com>
2025-06-26 19:44:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
@@ -9,33 +9,6 @@
 class Solution {
 public:
     void deleteNode(ListNode* node) {
-        ListNode* temp=node;
-        while(temp->next!=nullptr){
-            temp->val=temp->next->val;
-
-            cout<<temp->val;
-            
-            if(temp->next->next==nullptr){
-                temp->next=temp->next->next;
-                break;
-            }
-            temp=temp->next;
-
-        }
-       // temp.delete();
-        //cout<<temp->val;
         
-        //return node;
     }
 };
-/* WOWOW SOLn;
-class Solution {
-public:
-    void deleteNode(ListNode* node) {
-        // Overwrite data of next node on current node.
-        node->val = node->next->val;
-        // Make current node point to next of next node.
-        node->next = node->next->next;
-    }
-};
-*/
