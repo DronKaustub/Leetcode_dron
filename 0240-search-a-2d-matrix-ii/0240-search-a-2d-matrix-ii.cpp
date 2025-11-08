d568e11168d103b2ae9568f006769efa1ef5a1fb
Dron Kaustub <97157814+DronKaustub@users.noreply.github.com>
2025-11-08 21:29:55 +0530

    Time: 78 ms (20.26%), Space: 18.7 MB (68.24%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -0,0 +1,34 @@
+
+class Solution {
+public:
+    bool searchArray(vector<int>& matrixArr, int target,int high){
+        int low=0;
+        while(high>=low){
+            int mid= low+(high-low)/2;
+            if(matrixArr[mid]==target){
+                return true;
+            }
+            else if(matrixArr[mid]>target){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return false;
+    }
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n= matrix.size();
+        int m=matrix[0].size();
+        bool ans=false;
+        for(int i =0;i<n;i++){
+            if((matrix[i][0]<=target)&&(matrix[i][m-1]>=target)){
+                ans=searchArray(matrix[i],target,m);
+                if(ans==true){
+                    return true;
+                }
+            }
+        }
+        return ans;
+    }
+};
